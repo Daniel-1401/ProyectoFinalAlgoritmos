8eb3f92748f9513567e9e7303b282b805e8541c5
Mathias <i202012008@cibertec.edu.pe>
2020-12-05 23:30:11 -0500

    GUI DlgReporte_Matricula_Curso.java, DlgReporte_Matricula_Pendiente.java, DlgReporte_Matricula_Vigente.java TERMINADO

--- ProyectoFinal/src/Arreglos/ArregloAlumno.java
+++ ProyectoFinal/src/Arreglos/ArregloAlumno.java
@@ -16,6 +16,8 @@ public class ArregloAlumno {
 		cargarAlumnos();
 	}
 	
+	//Operaciones Básicas
+	
 	public void adicionar (Alumno x) {
 		alumno.add(x);
 		grabarAlumnos();
@@ -55,6 +57,7 @@ public class ArregloAlumno {
 			// TODO: handle exception
 		}
 	}
+	 
 	
 	private void grabarAlumnos() {
 		OrdenarLista();
@@ -81,6 +84,9 @@ public class ArregloAlumno {
 		catch (Exception e) {
 		}
 	}
+	
+	//Se cargan los datos almacenados en los archivos .txt
+	
 	private void cargarAlumnos() {
 		try {
 			BufferedReader br;
